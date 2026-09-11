6a998ab64f618acd6d2ba430fcce66903dc85187
Kaztaztrophe <kaztaztrophe@gmail.com>
2026-09-11 01:00:25 -0400

    Update wolvhelper_encounters.user.js

--- wolvhelper_encounters.user.js
+++ wolvhelper_encounters.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Wolvhelper: Explore Encounters
 // @namespace   https://github.com/Kaztaztrophe/Wolvhelper/
-// @version     1.0.1
+// @version     1.0.2
 // @author      Kaztaztrophe
 // @description Wolvden explore encounter helper which displays results
 // @match       https://www.wolvden.com/*
